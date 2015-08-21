aee17e622cee80ce11aac10300a1404ea7481af6
ravjot <ravjot28@gmail.com>
2015-08-20 22:01:07 -0400

    Initial commit of ds2

---
+++ ds2/src/main/java/com/ds2/DBUtils.java
@@ -0,0 +1,33 @@
+package com.ds2;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtils {
+	private static DBUtils object;
+	private Connection connection;
+
+	private DBUtils() {
+
+	}
+
+	public static DBUtils getInstanct() {
+		if (object == null) {
+			object = new DBUtils();
+		}
+
+		return object;
+	}
+
+	public Connection getConnection() {
+		if (connection == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager.getConnection("jdbc:mysql://10.10.10.100/DS2?user=web&password=admin");
+			} catch (Exception e) {
+
+			}
+		}
+		return connection;
+	}
+
+}
